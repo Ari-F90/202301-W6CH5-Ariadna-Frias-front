ae84a624429d4e1b395fe9a00694e3ba79dafb32
Ariadna Frias <ariadna.812@gmail.com>
2023-02-26 20:34:35 +0100

    Update things list

---
+++ src/feature/things/components/things-list/things-list.tsx
@@ -0,0 +1,26 @@
+import { useThings } from "../../hooks/use.things";
+
+import { ThingApiRepo } from "../../services/repository/thing.api.repo";
+
+export function ThingsList() {
+  // eslint-disable-next-line new-parens
+  const { things } = useThings(new ThingApiRepo());
+  return (
+    <>
+      <h2>Things list</h2>
+      <section>
+        <ul>
+          {things.map((item) => (
+            <li key={item.id}>
+              {item.name}
+              <p>id: {item.id}</p>
+
+              <p>week: {item.week}</p>
+              <p>level: {item.level}</p>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </>
+  );
+}
