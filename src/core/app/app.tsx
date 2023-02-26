8557e71921beaeba8dea9e5a4f90168049e4776b
Ariadna Frias <ariadna.812@gmail.com>
2023-02-26 19:28:09 +0100

    Update app file

---
+++ src/core/app/app.tsx
@@ -0,0 +1,11 @@
+import { Header } from "../../components/header/header";
+function App() {
+  return (
+    <div className="App">
+      <span>Learn</span>
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
