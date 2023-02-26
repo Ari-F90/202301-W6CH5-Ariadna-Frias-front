5df08e510a4c5c134933a1dd8eaeab6fb2be1c9a
Ariadna Frias <ariadna.812@gmail.com>
2023-02-26 21:09:07 +0100

    Update app test

--- src/core/app/app.test.tsx
+++ src/core/app/app.test.tsx
@@ -4,13 +4,16 @@ import { Provider } from "react-redux";
 import App from "./app";
 import { store } from "../store/store";
 
-test("renders learn react link", () => {
-  render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
-
-  const element = screen.getByText(/Learn/i);
-  expect(element).toBeInTheDocument();
+describe("Given the app component", () => {
+  describe("When it is rendered", () => {
+    test("renders learn react link", () => {
+      render(
+        <Provider store={store}>
+          <App />
+        </Provider>
+      );
+      const element = screen.getByText(/Learn/i);
+      expect(element).toBeInTheDocument();
+    });
+  });
 });
